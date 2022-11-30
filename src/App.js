51f7ee1b725a849714c2053704a64db30a6101ec
Emeka1993 <brolin933@gmail.com>
2022-11-30 15:20:41 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
